814ef96d3c0909a3c8d91a78b2c3613c44688f7d
Carlos Liarte Navarro <jrcharly33@hotmail.com>
2015-11-24 19:54:46 +0100

    Nuevo commit

--- insti/src/YearUtilities.java
+++ insti/src/YearUtilities.java
@@ -3,14 +3,14 @@ public class YearUtilities {
 
 		public static boolean isLeap(int year){
 	        if(year%4 == 0){
-	            if(year%100 == 0){
+	            if(year%100 == 0){ //no bisiesto
 	                if(year%400 == 0){
 	                    return true; //modificacion de true a false
 	                }else{
 	                    return false;  
 	                }
 	            }else{
-	                return false;
+	                return true; //modificacion a true
 	            }
 	        }else{
 	            return false;//modificacion de false a true
